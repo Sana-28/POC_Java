39eaf88011401a1ac9f8cdbfae0aad9f6b1f82e9
Sana Shaikh <sana.shaikh@zensar.com>
2023-05-25 21:51:48 +0530

    java juint testing

---
+++ JavaJunit/src/main/java/org/example/todoImpl/TodoServiceImpl.java
@@ -0,0 +1,14 @@
+package org.example.todoImpl;
+
+import org.example.todoInter.TodoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoServiceImpl implements TodoService {
+    @Override
+    public List<String> retrieveTodos(String user) {
+        List<String> filteredTodos = new ArrayList<String>();
+        return null;
+    }
+}
